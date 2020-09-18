33fd7d54ba488632a8c66b2afce4e83da3d0f9c7
George Rautenbach <jansr16@gmail.com>
2020-09-18 13:07:50 +0200

    Counting working for https repos

--- counting-app/counting_app.rb
+++ counting-app/counting_app.rb
@@ -1,8 +1,45 @@
 # Main counting program
 
-if ARGV.empty?
-  puts 'Please provide the VCS url'
-  exit
+require 'rugged'
+require 'tmpdir'
+
+def main
+  if ARGV.empty?
+    puts 'Please provide the VCS url'
+    exit
+  end
+
+  vcs_url = ARGV[0]
+
+  # setup working dir
+  Dir.mktmpdir 'loc' do |ws|
+
+    # clone remote repo
+    Rugged::Repository.clone_at(vcs_url, ws)
+
+    puts lines_in_dir ws
+  end
+
 end
 
+# Do DFS line counting
+def lines_in_dir(dir)
+  total_lines = 0
 
+  Dir.open(dir).each_child do |filename|
+    full_file = dir + '/' + filename
+    if File.directory? full_file
+      total_lines += lines_in_dir(full_file) unless filename == '.git'  # ignore .git dir
+    else
+      File.open(full_file) do |file|
+        file.each do
+          total_lines += 1
+        end
+      end
+    end
+  end
+
+  total_lines
+end
+
+main
