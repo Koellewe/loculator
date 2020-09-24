2d3c5f3e742db6053d13abd8a956908aac898834
George Rautenbach <jansr16@gmail.com>
2020-09-24 21:48:50 +0200

    Added decent demo, docs page, and logo

--- web-server/README.md
+++ web-server/README.md
@@ -6,4 +6,12 @@ A pretty simple web server, serving as a wrapper for the counting app.
 
 Config for this web app is in json format. The file should be specified in the environment variable `LOC_WEB_CFG` and set before running the app. Example config can be found in `example_cfg.json`.
 
-//TODO: running and deployment
+## Running
+
+```shell script
+bin/rails serve
+```
+
+## Prod
+
+Deploy this rail however you want. Currently the CI does some very crude zipping, uploading, and serving with Passenger. 
--- web-server/app/controllers/pages_controller.rb
+++ web-server/app/controllers/pages_controller.rb
@@ -5,6 +5,15 @@ class PagesController < ApplicationController
     render template: 'pages/home'
   end
 
+  def docs
+    @title = 'LoCulator Documentation'
+    @host = Rails.application.config.cfg['host_domain']
+    pubkey_path = Rails.application.config.cfg['ssh_key_path'] + '/' +
+                  Rails.application.config.cfg['ssh_key_name'] + '.pub'
+    @public_key = File.read(pubkey_path)
+    render template: 'pages/docs'
+  end
+
   def not_found
     render file: 'public/404.html', status: :not_found
   end
